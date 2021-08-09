9ff71d7d6621dd1cebf91ba531e7f7b9d8fb3f9b
Hernandez <Kimberly.Hernandez@LibertyMutual.com>
2021-08-08 22:37:54 -0700

    second push

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
 
-class extends React Component {
-
+class Quiz extends Component {
+    constructor(props){
+        super(props)
+        this.state={quiz_position: 1}
+    }
+    render () { 
+        return(
+           <div><div className="QuizQuestion">{quizData.quiz_questions[0].instrution_text}</div></div>
+        )
+       }
 }
 
 export default Quiz
